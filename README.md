09ecdd8aba81514eef6c8737163fce0100286427
frisenbrink <fredrik.frisenbrink@revibeenergy.com>
2024-02-19 16:32:55 +0100

    Shell script uses the local motd .bash_aliases files.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # orange_gw_install
+Collection of scripts that automizes the Anura Orange GW install procedure.
--- orange_setup.sh
+++ orange_setup.sh
@@ -53,12 +53,12 @@ echo '**************************************'
 echo 'Setup .bash_aliases.'
 echo '**************************************'
 cd ..
-wget https://gist.githubusercontent.com/Frisenbrink/e9543de27e49a38bd37c23f2f8afff6b/raw/cf3b7efe088f5ed18d2e7357489fda9b8dbfdfbc/.bash_aliases
-#sudo mv .bash_aliases ~/.bash_aliases
+#wget https://gist.githubusercontent.com/Frisenbrink/e9543de27e49a38bd37c23f2f8afff6b/raw/cf3b7efe088f5ed18d2e7357489fda9b8dbfdfbc/.bash_aliases
+sudo mv .bash_aliases ~/.bash_aliases
 echo '**************************************'
 echo 'Download motd.'
 echo '**************************************'
-wget https://gist.githubusercontent.com/Frisenbrink/08234c269c7a5c81ca519802d2ec98b0/raw/b65a0b3e1d86fe7804199aee3cd6841f96d51bde/motd
+#wget https://gist.githubusercontent.com/Frisenbrink/08234c269c7a5c81ca519802d2ec98b0/raw/b65a0b3e1d86fe7804199aee3cd6841f96d51bde/motd
 sudo mv motd /etc/motd
 echo 'Cleanup files.'
 echo '**************************************'
